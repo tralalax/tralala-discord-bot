e3f12b916f004c6795f9c14795f33ff346cdf412
tralalax <timepro@gmx.com>
2023-02-19 17:34:26 +0100

    edit

--- __main__.py
+++ __main__.py
@@ -1,3 +1,29 @@
 import discord
 import asyncio
+import logging
 from discord.ext import commands
+
+# create bot instance
+bot = commands.Bot(command_prefix=command_prefix, description=description, case_insensitive=True, intents=discord.Intents.all())
+
+# create log handler for discord.py
+log_handler = logging.FileHandler(filename='discordBot.log', encoding='utf-8', mode='w')
+
+
+
+cogsCommand = ['cogs.test']
+
+if __name__ == '__main__':
+
+    '''load cogs'''
+
+    for extension in cogsCommand:
+        bot.load_extension(extension)
+
+
+@bot.event 
+async def on_ready():
+    print('Logged in as')
+    print(bot.user)
+    print('------')
+
